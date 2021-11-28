bf63ef12d9815b3cabbc12b89352648354f76fc9
Senthil Kumar <vsenth17@ford.com>
2021-11-28 20:35:34 +0530

    Handling Page layouuts

---
+++ components/Header.js
@@ -0,0 +1,6 @@
+
+function Header() {
+    return <div className='layout-header'> Header</div>
+}
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import '../styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  if(Component.getLayout){
+    return Component.getLayout(<Component {...pageProps} />) 
+  }
+
+  return (
+    <> 
+    <Header />
+    <Component {...pageProps} />
+    <Footer/> 
+    </>
+  )
 }
 
 export default MyApp
