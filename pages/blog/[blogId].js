0aeb03e18c9ef7b91817bb69bee853b9ef6e290a
Senthil Kumar <vsenth17@ford.com>
2021-12-03 10:13:15 +0530

    Using Env Variables

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -7,31 +7,47 @@ function Blog( {title, description}) {
              <title>{title}</title>
              <meta name='description' content='description'></meta>
         </Head>
-        <h1 className='content'>Article</h1>
+        <h1 className='content'>
+            Env User {process.env.DB_USER} and Analytics Id {process.env.NEXT_PUBLIC_ANALYTICS_ID} 
+        </h1>
         </>
     )
 }
 
 export default Blog
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
     
+
+//     return {
+//         props: {
+//             title: 'Article Title',
+//             description: 'Article description',
+//         },
+//     }
+// }
+
+// export async function getStaticPaths() {
+    
+
+//     return {
+//         paths: [{
+//             params: { blogId: '1'}
+//         }],
+//         fallback: false,
+//     }
+// }
+
+export async function getServerSideProps() {
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+
+console.log(`Connectig to Database with username ${user} and password ${password}`)
 
     return {
         props: {
             title: 'Article Title',
             description: 'Article description',
-        },
-    }
-}
-
-export async function getStaticPaths() {
-    
-
-    return {
-        paths: [{
-            params: { blogId: '1'}
-        }],
-        fallback: false,
+        }
     }
 }
