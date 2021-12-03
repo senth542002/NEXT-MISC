bb84999304d7dc4a3634f01b65b4690c3ac1b7ef
Senthil Kumar <vsenth17@ford.com>
2021-12-03 09:36:33 +0530

    Next js configuring Redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  redirects: async() => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: false,
+      }
+    ]
+  }
 }
